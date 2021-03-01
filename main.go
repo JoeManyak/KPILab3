457956e046ff6e747a34f75110bda748b1a13ffc
Joe <doncrafttv@gmail.com>
2021-03-01 16:07:50 +0200

    read path from file

--- main.go
+++ main.go
@@ -1,7 +1,28 @@
 package main
 
-import "fmt"
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strings"
+)
 
 func main() {
-	fmt.Println("Init")
+	f, err := os.Open("input.txt")
+	if err != nil {
+		panic(err.Error())
+	}
+	defer f.Close()
+	scanner := bufio.NewScanner(f)
+	var inpMap [][]string
+	for scanner.Scan() {
+		text := scanner.Text()
+		inpMap = append(inpMap, strings.Split(text, ""))
+	}
+	for i := range inpMap {
+		for j := range inpMap[i] {
+			fmt.Printf("%2s", inpMap[i][j])
+		}
+		fmt.Println()
+	}
 }
