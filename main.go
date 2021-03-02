76f19fb47017d450f09136dae4a41e3ef35ab988
Joe <doncrafttv@gmail.com>
2021-03-02 16:44:14 +0200

    alpha

--- main.go
+++ main.go
@@ -13,8 +13,10 @@ type cell struct {
 	thisY       int
 	parentX     int
 	parentY     int
+	parent      *cell
 	parentsCost float64
-	thisCost    float64
+	fullCost    float64
+	roadCost    float64
 }
 
 func toSquare(a int) int {
@@ -23,36 +25,133 @@ func toSquare(a int) int {
 
 func (c *cell) calculateTotalCost(aimX, aimY int) {
 	costToAim := math.Sqrt(float64(toSquare(aimX-c.thisX) + toSquare(aimY-c.thisY)))
-	c.thisCost = costToAim + c.parentsCost
+	c.fullCost = c.roadCost + costToAim
+}
+
+func (c *cell) roadCostCalculate() {
+	costToNeighbor := math.Sqrt(float64(toSquare(c.parentX-c.thisX) + toSquare(c.parentY-c.thisY)))
+	c.roadCost = costToNeighbor + c.parentsCost
 }
 
 func main() {
 	inpMap := readPathFromFile("input.txt")
 	showPath(inpMap)
-	pathFinder(inpMap, 1, 1, 2, 2)
+	pathFinder(inpMap, 1, 1, 3, 6)
 }
 
-func initCell() cell {
+func initCell(x, y int) cell {
 	var c cell
-	c.thisX = 1
-	c.thisY = 1
+	c.thisX = x
+	c.thisY = y
 	c.parentX = -1
 	c.parentY = -1
 	c.parentsCost = 0
+	c.fullCost = 0
 	return c
 }
 
 // x1,y1 — start of path
 // x2,y2 — end of path
 
-func pathFinder(inpMap [][]string, x1, y1, x2, y2 int) {
-	c := initCell()
+func pathFinder(inpMap [][]string, startX, startY, aimX, aimY int) ([]cell, []cell, string) {
+	c := initCell(startX, startY)
 	openList := make([]cell, 0, 0)
 	closedList := make([]cell, 0, 0)
 	openList = append(openList, c)
-	if inpMap[x1][y1] == "X" || inpMap[x2][y2] == "X" {
-		panic("Not Valid start")
+	for {
+		selected := getCheapestCell(openList)
+		if checkIfSuccess(selected.thisX, selected.thisY, aimX, aimY) {
+			return openList, closedList, "FAILURE"
+		}
+		deleteCellFromSlice(openList, findIndexOfCell(selected, openList))
+		closedList = append(closedList, selected)
+		for _, v := range getNotClosedNeighbors(&selected, closedList, openList, inpMap, aimX, aimY) {
+			temp := math.Sqrt(float64(toSquare(v.thisX-c.thisX) + toSquare(v.thisY-c.thisY)))
+			if findIndexOfCell(c, openList) == -1 || temp < v.roadCost {
+				v.parent = &c
+				v.roadCost = temp
+				v.calculateTotalCost(aimX, aimY)
+				if findIndexOfCell(c, openList) == -1 {
+					openList = append(openList, v)
+				}
+			}
+		}
 	}
+	//return openList,closedList, "HORRAY"
+}
+
+func findIndexOfCell(c cell, cArr []cell) int {
+	for i := range cArr {
+		if c == cArr[i] {
+			return i
+		}
+	}
+	return -1
+}
+
+func deleteCellFromSlice(c []cell, i int) []cell {
+	temp := make([]cell, len(c)-1, len(c)-1)
+	copy(temp[:i], c[:i])
+	copy(temp[i:], c[i+1:])
+	c = temp
+	return c
+}
+
+func getNotClosedNeighbors(c *cell, closedList, openList []cell, initMap [][]string, aimX, aimY int) []cell {
+	cellArr := make([]cell, 0, 0)
+	for i := c.thisX - 1; i < c.thisX+2; i++ {
+		for j := c.thisY - 1; j < c.thisY+2; j++ {
+			if i == 1 && j == 1 {
+				continue
+			}
+			if initMap[i][j] != "X" {
+				var b = true
+				for item := range closedList {
+					if closedList[item].thisX == i && closedList[item].thisY == j {
+						b = false
+						break
+					}
+				}
+				for item := range openList {
+					if openList[item].thisX == i && openList[item].thisY == j {
+						b = false
+						cellArr = append(cellArr, openList[item])
+						break
+					}
+				}
+				if b {
+					cellArr = append(cellArr, cell{
+						thisX:       i,
+						thisY:       j,
+						parentX:     c.thisX,
+						parentY:     c.thisY,
+						parent:      c,
+						parentsCost: c.fullCost,
+					})
+					cellArr[len(cellArr)-1].roadCostCalculate()
+					cellArr[len(cellArr)-1].calculateTotalCost(aimX, aimY)
+				}
+			}
+		}
+	}
+	return cellArr
+}
+
+func checkIfSuccess(x1, y1, x2, y2 int) bool {
+	return (math.Abs(float64(x1-x2)) <= 1) && (math.Abs(float64(y1-y2)) <= 1)
+}
+
+func getCheapestCell(openList []cell) cell {
+	if len(openList) == 0 {
+		panic("Empty open list!")
+	}
+	var minCell = openList[0]
+	for i := range openList {
+		if minCell.fullCost > openList[i].fullCost {
+			minCell = openList[i]
+		}
+	}
+	return minCell
 }
 
 func readPathFromFile(filename string) [][]string {
