8565d52a69ce96b8ff0db6d3d98dc9af9e56cb7e
Joe <doncrafttv@gmail.com>
2021-03-02 13:33:15 +0200

    initialize lists

--- main.go
+++ main.go
@@ -9,11 +9,12 @@ import (
 )
 
 type cell struct {
-	thisX     int
-	thisY     int
-	parentX   int
-	parentY   int
-	totalCost float64
+	thisX       int
+	thisY       int
+	parentX     int
+	parentY     int
+	parentsCost float64
+	thisCost    float64
 }
 
 func toSquare(a int) int {
@@ -21,9 +22,8 @@ func toSquare(a int) int {
 }
 
 func (c *cell) calculateTotalCost(aimX, aimY int) {
-	costToParent := math.Sqrt(float64(toSquare(c.parentX-c.thisX) + toSquare(c.parentY-c.thisY)))
 	costToAim := math.Sqrt(float64(toSquare(aimX-c.thisX) + toSquare(aimY-c.thisY)))
-	c.totalCost = costToParent + costToAim
+	c.thisCost = costToAim + c.parentsCost
 }
 
 func main() {
@@ -32,10 +32,24 @@ func main() {
 	pathFinder(inpMap, 1, 1, 2, 2)
 }
 
+func initCell() cell {
+	var c cell
+	c.thisX = 1
+	c.thisY = 1
+	c.parentX = -1
+	c.parentY = -1
+	c.parentsCost = 0
+	return c
+}
+
 // x1,y1 — start of path
 // x2,y2 — end of path
 
 func pathFinder(inpMap [][]string, x1, y1, x2, y2 int) {
+	c := initCell()
+	openList := make([]cell, 0, 0)
+	closedList := make([]cell, 0, 0)
+	openList = append(openList, c)
 	if inpMap[x1][y1] == "X" || inpMap[x2][y2] == "X" {
 		panic("Not Valid start")
 	}
